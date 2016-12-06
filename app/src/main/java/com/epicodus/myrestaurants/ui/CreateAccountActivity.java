5b5963fe3bafea506d2443033dd1a92d83902c38
Sheena Nickerson <sheenanick@gmail.com>
2016-12-06 13:07:26 -0800

    add click listener to start login activity

--- app/src/main/java/com/epicodus/myrestaurants/ui/CreateAccountActivity.java
+++ app/src/main/java/com/epicodus/myrestaurants/ui/CreateAccountActivity.java
@@ -1,15 +1,32 @@
 package com.epicodus.myrestaurants.ui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.epicodus.myrestaurants.R;
 
-public class CreateAccountActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class CreateAccountActivity extends AppCompatActivity implements View.OnClickListener {
+    @Bind(R.id.loginTextView) TextView mLoginTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+        ButterKnife.bind(this);
+        mLoginTextView.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == mLoginTextView) {
+            Intent intent = new Intent(CreateAccountActivity.this, LoginActivity.class);
+            startActivity(intent);
+        }
     }
 }
